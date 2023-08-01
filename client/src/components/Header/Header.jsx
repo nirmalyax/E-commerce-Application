0af5569472b142301211b0b9f7f98feb431f1fef
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-08-01 12:05:39 +0530

    first commit

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+
+import Search from "./Search/Search";
+import { Context } from "../../utils/context";
+import Cart from "../Cart/Cart";
+
+import "./Header.scss";
+
+const Header = () => {
+    return <header className="main-header">
+        <div className="header-content">
+            <ul className="left">
+                <li>Home</li>
+                <li>About</li>
+                <li>Categories</li>
+            </ul>
+            <div className="center">E-kart.</div>
+            <div className="right"></div>
+        </div>
+    </header>;
+};
+
+export default Header;
