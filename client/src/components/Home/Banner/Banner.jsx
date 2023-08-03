23254fed9c29a6e53197d63f22ac294895628b82
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-08-03 19:44:00 +0530

    commit

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,20 @@
 import "./Banner.scss";
+
+import BannerImg from "../../../assets/banner-img.png"
+
 const Banner = () => {
-    return <div>Banner</div>;
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>SALES</h1>
+                    <p></p>
+                </div>
+                <img src={BannerImg} alt="" />
+            </div>
+            
+        </div>
+    );
 };
 
 export default Banner;
