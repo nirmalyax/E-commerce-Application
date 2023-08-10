21891b7b4ac88e2fab342f362e57079dab8de8b2
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-08-10 14:34:32 +0530

    c

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,69 @@
+import React from "react";
 import "./Footer.scss";
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <div className="footer">
+            <div className="footer-content">
+                <div className="col">
+                    <div className="title">About</div>
+                    <div className="text">
+                        Voluptatem accusantium doloremque laudantium, totam rem
+                        aperiam, eaque ipsa quae ab illo inventore veritatis et
+                        quasi architecto beatae vitae dicta sunt explicabo eaque
+                        ipsa quae ab illo.
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Contact</div>
+                    <div className="c-item">
+                        <FaLocationArrow />
+                        <div className="text">
+                            MG Road, Kolkata, <br />
+                            West Bengal, 700090
+                        </div>
+                    </div>
+                    <div className="c-item">
+                        <FaMobileAlt />
+                        <div className="text">Phone: 0471 272 0261 <br /> </div>
+                    </div>
+                    <div className="c-item">
+                        <br />
+                        <FaEnvelope />
+                        <div className="text">  Email: store@doorstep.com</div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Categories</div>
+                    <span className="text">Headphones</span>
+                    <span className="text">Smart Watches</span>
+                    <span className="text">Bluetooth Speakers</span>
+                    <span className="text">Wireless Earbuds</span>
+                    <span className="text">Home Theatre</span>
+                    <span className="text">Projectors</span>
+                </div>
+                <div className="col">
+                    <div className="title">Pages</div>
+                    <span className="text">Home</span>
+                    <span className="text">About</span>
+                    <span className="text">Privacy Policy</span>
+                    <span className="text">Returns</span>
+                    <span className="text">Terms & Conditions</span>
+                    <span className="text">Contact Us</span>
+                </div>
+            </div>
+            <div className="bottom-bar">
+                <div className="bottom-bar-content">
+                    <span className="text">
+                        DOORSTEP. 2023 CREATED BY NIRMALYA. PREMIUM E-COMMERCE
+                        SOLUTIONS.
+                    </span>
+                    <img src={Payment} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
