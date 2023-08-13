d80873685000c1a662988d3b4d4d91735a2cf0bd
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-08-13 07:22:54 +0530

    c

--- client/src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
@@ -1,5 +1,17 @@
-const RelatedProducts = () => {
-    return <div>Related Products</div>;
+import React from "react";
+import useFetch from "../../../hooks/useFetch";
+import Products from "../../Products/Products";
+
+const RelatedProducts = ({ categoryId, productId }) => {
+    const { data } = useFetch(
+        `/api/products?populate=*&filters[id][$ne]=${productId}&filters[categories][id]=${categoryId}&pagination[start]=0&pagination[limit]=4`
+    );
+
+    return (
+        <div className="related-products">
+            <Products headingText="Related Products" products={data} />
+        </div>
+    );
 };
 
 export default RelatedProducts;
